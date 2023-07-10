8a93b396b66970b9accdcb837dd7bec68eb1d4c5
LAKSHMI PRASANNA TANGUTURI <88602508+prasanna61@users.noreply.github.com>
2023-07-10 08:50:03 +0530

    question 1

--- CarModels.java
+++ CarModels.java
@@ -1,11 +1,11 @@
 package Mainproject;
 public class CarModels {
     public static void main(String[] args) {
-        String [] carmodel= {"Polo Trendline","Polo Highline","Virtus Trendline","Virtus Highline","Taigun Trendline","Taigun Highline","Taigun Topline"};
+        String [] model= {"Polo Trendline","Polo Highline","Virtus Trendline","Virtus Highline","Taigun Trendline","Taigun Highline","Taigun Topline"};
 		System.out.println("carmodels are");
-        for(int i=0;i<carmodel.length;i++)
+        for(int i=0;i<model.length;i++)
 		{
-			System.out.println(carmodel[i]);
+			System.out.println(model[i]);
         }	
     }
 }
