79a3b6cc4a66fbfc79b8dd72cf8c4c6aec25afec
LAKSHMI PRASANNA TANGUTURI <88602508+prasanna61@users.noreply.github.com>
2023-07-15 12:01:13 +0530

    Update CarInformation.java

--- CarModels.java
+++ CarModels.java
@@ -1,11 +1,11 @@
 package Mainproject;
-public class CarModels {
+public class CarInformation {
     public static void main(String[] args) {
-        String [] model= {"Polo Trendline","Polo Highline","Virtus Trendline","Virtus Highline","Taigun Trendline","Taigun Highline","Taigun Topline"};
+        String [] Car_models= {"Polo Trendline","Polo Highline","Virtus Trendline","Virtus Highline","Taigun Trendline","Taigun Highline","Taigun Topline"};
 		System.out.println("carmodels are");
-        for(int i=0;i<model.length;i++)
+        for(int i=0;i<Car_models.length;i++)
 		{
-			System.out.println(model[i]);
+			System.out.println(Car_models[i]);
         }	
     }
 }
